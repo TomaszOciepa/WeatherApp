ea16fbe2fca13324d2df2de4dc1a2287e209e388
tomaszociepa <tom.ociepa@gmail.com>
2019-08-07 21:30:43 +0200

    add freeMarker and start app servlet

---
+++ src/main/java/servlet/IndexServlet.java
@@ -0,0 +1,39 @@
+package servlet;
+
+import freeMarker.TemplateProvider;
+import freemarker.template.Template;
+import freemarker.template.TemplateException;
+
+import javax.inject.Inject;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+import java.util.Map;
+
+@WebServlet(urlPatterns = ("/"))
+public class IndexServlet extends HttpServlet {
+
+    @Inject
+    private TemplateProvider templateProvider;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        resp.addHeader("Content-Type", "text/html; charset=utf-8");
+        PrintWriter out = resp.getWriter();
+        Template template;
+        Map<String, Object> model = new HashMap<>();
+
+        template = templateProvider.getTemplate(getServletContext(), "index");
+        try {
+            template.process(model, out);
+        } catch (TemplateException e) {
+            e.printStackTrace();
+        }
+    }
+}
